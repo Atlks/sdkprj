290a58044c9056e7e9569b578076fccc13292255
jark <kark@kk.com>
2023-07-14 13:25:07 +0700

    tlgrm msg hdl

--- tlgrm_tmp/tlgrmHdl.php
+++ tlgrm_tmp/tlgrmHdl.php
@@ -1,41 +1,42 @@
 <?php
 
 //  C:\phpstudy_pro\Extensions\php\php7.4.3nts\php.exe    C:\w\jbbot\tlgrm.php
-$chat_id=-960237539;
-$bot_token="5464498785:AAGtLv-M-RKgRoIh5G3XEfkdqkCPiVBB1NA";
+$chat_id = -960237539;
+$bot_token = "5464498785:AAGtLv-M-RKgRoIh5G3XEfkdqkCPiVBB1NA";
 
 require __DIR__ . '/vendor/autoload.php';
 $bot = new \TelegramBot\Api\BotApi($bot_token);
 //$bot->sendmessage($chat_id, "hahtxt");
 
- 
 
-$fname=$_SERVER ['argv'][1];
 
-$msgg=file_get_contents($fname.".json");
+$fname = $_SERVER['argv'][1];
+
+$msgg = file_get_contents($fname . ".json");
 $json = json_decode($msgg, true); // decode the JSON into an associative array
-$msg_txt=$json ['text'];
+$msg_txt = $json['text'];
 
 
 
 if ($msg_txt == "余额") {
-    $uid=$json["from"]["id"];
-    $fstname=$json["from"]["first_name"];
-    $sendmsg=urlencode("用户ID: $uid \r\n用户名:  $fstname \r\n余额: 0 \r\n输赢: 0");
-    $msg_url_tlgrm= "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$sendmsg";
-echo $msg_url_tlgrm ;
-//send
-file_get_contents($msg_url_tlgrm);
-
+    $uid = $json["from"]["id"];
+    $fstname = $json["from"]["first_name"];
+    $sendmsg ="用户ID: $uid \r\n用户名:  $fstname \r\n余额: 0 \r\n输赢: 0";
+    echo  $sendmsg;
+    $msg_urlcode= urlencode($sendmsg );
+    $msg_url_tlgrm = "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$msg_urlcode";
+    echo $msg_url_tlgrm;
+    //send
+    file_get_contents($msg_url_tlgrm);
 }
 
 
 
-function sendmsg($url,$msg)
+function sendmsg($url, $msg)
 {
-    $url_tmp="https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$msg";
-echo $url_tmp;
+    $url_tmp = "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=$msg";
+    echo $url_tmp;
 
-echo file_get_contents($url_tmp);
+    echo file_get_contents($url_tmp);
 }
 //{"id":-960237539,"title":"grptst","type":
--- tlgrm_tmp/tlgrm_rcvMsg.js
+++ tlgrm_tmp/tlgrm_rcvMsg.js
@@ -2,47 +2,83 @@
 //   https://api.telegram.org/bot5464498785:AAGtLv-M-RKgRoIh5G3XEfkdqkCPiVBB1NA/getUpdates
 ////////   npm install node-telegram-bot-api
 
-
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
+$phpexe = "C:\\phpstudy_pro\\Extensions\\php\\php7.4.3nts\\php.exe";
+$tlghr_msg_hdl = " C:\\w\\jbbot\\tlgrmHdl.php ";
 const token = '5464498785:AAGtLv-M-RKgRoIh5G3XEfkdqkCPiVBB1NA';
 
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, { polling: true });
 
 
+bot = getTelegramBot(token);
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+
 bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
 
-    $msgx = msg.text;
-
-    /*
-     if ($msx == "余额") {、、
-        // 用户ID: 879006550 用户名: 十 jarkas 刘洋 汤姆 余额: 0 输赢: 0,
-    }
-     */
-
-
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+    //   $msgx = msg.text;
     console.log(msg);
+    bot.sendMessage(msg.chat.id, 'Received your message');
 
 
-    var fs = require("fs");
     $fname = Math.random();
-    fs.writeFileSync($fname + ".json", JSON.stringify(msg));
-
-    const { exec } = require('child_process');
-    var execSync = require('child_process').execSync;
-
-
-    $cmd = "C:\\phpstudy_pro\\Extensions\\php\\php7.4.3nts\\php.exe  C:\\w\\jbbot\\tlgrmHdl.php " + $fname;
-    console.log($cmd);
-
-    execSync($cmd);
+    writeFileSyncx($fname, JSON.stringify(msg));
+    execSyncx($phpexe + " " + $tlghr_msg_hdl + " " + $fname);
     console.log(999)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getTelegramBot(token) {
+    const TelegramBot = require('node-telegram-bot-api');
+
+    // replace the value below with the Telegram token you receive from @BotFather
+
+
+    // Create a bot that uses 'polling' to fetch new updates
+    const bot = new TelegramBot(token, { polling: true });
+
+    return bot;
+}
+
+
+
+function writeFileSyncx(fil, str) {
+    var fs = require("fs");
+    fs.writeFileSync($fname + ".json", str);
+}
+
+
+
+function execSyncx(cmd) {
+
+    console.log(cmd);
+    const { exec } = require('child_process');
+    var execSync = require('child_process').execSync;
+    execSync(cmd)
+}
