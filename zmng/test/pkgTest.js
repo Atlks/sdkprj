b00f7352fc0c6fb6e67d38adc8eb654c55660f4a
jark <kark@kk.com>
2023-08-31 22:39:39 +0700

    ...

---
+++ zmng/test/pkgTest.js
@@ -0,0 +1,18 @@
+// require("php")
+//
+// echo(999)
+
+
+require("ati1990_fp")
+
+echo(888)
+
+require("esm-hook");
+const chalk = require('chalk').default
+console.log(chalk.blue('你好'))
+
+console.log(chalk.bgYellow(chalk.blue('你好')))
+
+
+
+echo( json_encode(159) )
